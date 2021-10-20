45703303e97e7ce09567a82a3c00b643c5d7cda1
O'Cyrus JB <s1291542@monmouth.edu>
2021-10-20 16:07:23 -0400

    first commit
    check. out header issue

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Travelbaby</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+        <Header>
+        
+        </Header>
+      {/* Feed */}
+
+      {/* Modal */}
+
+    </div>
+  )
+}
